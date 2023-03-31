bdd3b6862c190e7ac296306f72751b0f8c32de8c
AkshayRitu <52860231+AkshayRitu@users.noreply.github.com>
2023-03-31 19:04:06 +0530

    Update Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -16,6 +16,7 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         double d_number1, d_number2;
         int i_number1;
+        // this is a comment
         // THis is a java comment
         // THis is a comment
         // When the Docker container is started in the background via Ansible and you attach your terminal to it,
